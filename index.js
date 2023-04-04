14489289b9cb99e35df8ad3a90b519d8b3c9ae88
ramanakorada2611 <koradaramana2611@gmail.com>
2023-04-04 16:52:01 +0530

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/data',(req,res)=>{
 const data1=require('./page/data')
 app.use('/',data1)
 const cors=require('cors')
-app.use(cors({origin:['http://10.170.63.237','http://blogpost.com']}))
+app.use(cors())
 app.listen(8080,()=>{
     console.log("server running at port 8080")
 })
