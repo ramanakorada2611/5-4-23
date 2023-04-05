64c76a67ab4a96958c2367d6c0863c5bf0e1b30b
ramanakorada2611 <koradaramana2611@gmail.com>
2023-04-05 09:18:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express=require("express")
+const cors=require('cors')
 const app=express()
-
+app.use(cors())
 app.get('/data',(req,res)=>{
     res.send({message:"req from data page"})
     console.log("data page")
 })
 const data1=require('./page/data')
 app.use('/',data1)
-const cors=require('cors')
-app.use(cors())
+
+
+
 app.listen(8080,()=>{
     console.log("server running at port 8080")
 })
