039182c88ecb39b209ceb2c638fb2bd76ac5dba6
ramanakorada2611 <koradaramana2611@gmail.com>
2023-04-05 09:56:55 +0530

    first commit

--- controller/data.js
+++ controller/data.js
@@ -3,7 +3,7 @@ const { findAllFromDb } = require("../database/connection")
  const dataPage=async(req,res)=>{
       try {
         const dataPage=await findAllFromDb({},"dataPage")
-        return  res.status(200).send({dataPage:dataPage})
+        return  res.status(200).send(dataPage)
       } catch (error) {
         console.log("Error occur in  fetching data",error)
        return res.status(500).send({message:"something went wrong"})
